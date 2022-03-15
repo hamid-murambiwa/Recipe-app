957b73a3f2b98027a1fc0c1725af8b8bdd9bb19a
adamteddychang <adamchang0725@gmail.com>
2022-03-15 18:36:31 +0100

    Fixed routing issues and linters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,4 @@
 class Recipe < ApplicationRecord
-
-
-
   belongs_to :user, class_name: 'User'
 
   has_many :recipe_foods
--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,17 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-#   resources :users, only: %i[index]
-#   resources :recipes, only: %i[index show new create destroy]
+  #   resources :users, only: %i[index]
+  resources :recipes, only: %i[index show new create destroy]
 
   resources :users, only: [:create] do
     resources :foods, only: %i[index new create destroy]
-    resources :recipes, only: %i[index show new create destroy]
+    # resources :recipes, only: %i[index show new create destroy]
   end
-
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
 
   root 'foods#index'
-
 end
