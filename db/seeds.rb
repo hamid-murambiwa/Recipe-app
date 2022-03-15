5dcf73b946b22cd92068f80642bdf16d187c2324
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-15 15:55:19 +0200

    Applications update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-user1 = User.create(name: 'hamid', email: 'hamid@gmail.com', password: '123456')
 
-food1 = Food.create(user_id: 1, name: 'grapes', unit: 'grams', price: 4)
-food2 = Food.create(user_id: 1, name: 'banana', unit: 'grams', price: 1)
+food1 = Food.create(user_id: 6, name: 'grapes', unit: 'grams', price: 4)
+food2 = Food.create(user_id: 6, name: 'banana', unit: 'grams', price: 1)
