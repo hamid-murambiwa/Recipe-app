381d82740305561234b958a42a52f6052d89b57f
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-15 15:45:49 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,20 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  get 'recipe/index'
-  get 'recipe/show'
-  get 'food/index'
-  get 'food/create'
-  get 'food/destroy'
-  get 'users/index'
-  get 'users/show'
+  resources :users, only: [:create] do
+    resources :foods, only: [:index, :new, :create, :destroy]
+    resources :recipes, only: [:index, :show]
+  end
+
+  # get 'recipe/index'
+  # get 'recipe/show'
+  # get 'foods/index'
+  # get 'foods/create'
+  # get 'foods/destroy'
+  # get 'users/index'
+  # get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
+  root 'foods#index'
 end
