16e378fbec9173aa04e7e2c1b915736166ccd08b
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-15 15:48:46 +0200

    Setup foods controller actions

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,17 +1,40 @@
 class FoodsController < ApplicationController
-  def index
-    @foods = Food.all
-  end
+    def index
+        @foods = Food.where(user_id: current_user.id)
+    end
 
-  def create
+    def new
+        @categories = ['grams', 'milligrams', 'killograms', 'pound', 'ounce', 'litre', 'cup', 'teaspoon', 'spoon', 'fluid ounce']
+    end
 
-  end
+    def show
+        @user = User.find(params[:user_id])
+        @foods = Food.all
+    end
+    
+    def create
+        @food = Food.new(mass_params)
+        @food.user_id = params[:user_id]
 
-  def destroy; end
-
-  private
-
-  def mass_params
-    params.require(:food).permit(:name, :price, :unit)
-  end
+        if @food.save
+            redirect_to user_foods_path
+        else
+            render :new
+        end
+    end
+    
+    def destroy
+        @food = Food.find(params[:id])
+        @user = User.find(params[:user_id])
+        @food.destroy!
+        @user.save
+        redirect_to user_foods_path
+        flash[:success] = 'food was deleted!'
+    end
+    
+      private
+    
+      def mass_params
+        params.require(:food).permit(:name, :unit, :price)
+      end
 end
