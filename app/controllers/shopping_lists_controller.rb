fd6d2e29387871776ead67e360e20537a6927e55
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-17 12:32:03 +0200

    Fix linters errors

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -3,12 +3,10 @@ class ShoppingListsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_foods = RecipeFood.where(recipe_id: params[:recipe_id])
     @food = Food.all
-    @total_price = 0;
-    @total_quantity = 0;
+    @total_price = 0
+    @total_quantity = 0
     @recipe_foods.each do |recipe_food|
       @total_price += recipe_food.food.price * recipe_food.quantity
-    end
-    @recipe_foods.each do |recipe_food|
       @total_quantity += recipe_food.quantity
     end
   end
