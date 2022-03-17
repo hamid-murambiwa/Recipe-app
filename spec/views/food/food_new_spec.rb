a4932e0c304c16fb616a79885c7c44f33d6fa535
hamid-murambiwa <machipisajunior@gmail.com>
2022-03-17 15:53:51 +0200

    Add food specs

--- spec/views/food/food_new_spec.rb
+++ spec/views/food/food_new_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe 'food/show.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'food', type: :feature do
+  describe 'new page' do
+    before(:each) do
+      visit user_session_path
+      @user = User.create!(name: '1',
+                           email: '1@first.com',
+                           password: '111111',
+                           confirmed_at: Time.now)
+
+      within('#new_user') do
+        fill_in 'Email', with: '1@first.com'
+        fill_in 'Password', with: '111111'
+      end
+      click_button 'Log in'
+      visit new_user_food_path(@user.id)
+    end
+
+    it 'New page shows the right content' do
+      expect(page).to have_content('Add Food')
+      expect(page).to have_content('Measuring unit')
+      expect(page).to have_content('Price')
+      expect(page).to have_content('Name')
+    end
+  end
 end
