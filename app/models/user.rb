cfa4811f5542c1cdf055911ddbff3ab88ffb3938
adamteddychang <adamchang0725@gmail.com>
2022-03-17 17:36:44 +0100

    removed confirmable so users can sign in without confirming their account

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   def set_role
     update(role: 'user')
