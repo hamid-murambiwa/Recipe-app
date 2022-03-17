bea14e7b0eea88ddfb3787df00c8d87c497c9597
adamteddychang <adamchang0725@gmail.com>
2022-03-17 09:11:39 +0100

    completed shopping lists

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # resources :users, only: %i[index]
   resources :recipes, only: %i[index show new create destroy] do
     resources :recipe_foods, only: %i[new create destroy update edit]
+    resources :shopping_lists, only: %i[index]
   end
 
   resources :users, only: [:create] do
