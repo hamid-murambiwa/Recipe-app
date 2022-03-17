ebdea5d6ec35aa777126d8ef331156674d7484c9
adamteddychang <adamchang0725@gmail.com>
2022-03-17 10:15:52 +0100

    completed shopping list table

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,7 +1,7 @@
 class ShoppingListsController < ApplicationController
   def index
     @recipe = Recipe.find(params[:recipe_id])
-    @recipe_foods = RecipeFood.all
+    @recipe_foods = RecipeFood.where(recipe_id: params[:recipe_id])
     @food = Food.all
   end
 end
